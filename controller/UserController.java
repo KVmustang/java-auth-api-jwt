e69d1786627520b626f59080b9a47175ed6aebcd
KelvinV <kelvinvaz77@gmail.com>
2026-01-28 07:54:38 -0500

    Create UserController.java

---
+++ controller/UserController.java
@@ -0,0 +1,13 @@
+package com.seunome.authapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/api/user/profile")
+    public String profile() {
+        return "This is a protected route";
+    }
+}

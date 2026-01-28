5aafe5c8a7a42513d2c035ac137536f0717abe96
KelvinV <kelvinvaz77@gmail.com>
2026-01-28 07:53:17 -0500

    Create JwtFilter.java

---
+++ security/JwtFilter.java
@@ -0,0 +1,31 @@
+package com.seunome.authapi.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    public JwtFilter(JwtService jwtService) {
+        this.jwtService = jwtService;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws java.io.IOException, jakarta.servlet.ServletException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            jwtService.extractUsername(token);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
